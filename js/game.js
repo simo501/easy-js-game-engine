93647eaf3a36ff06b3525573150fe090e5f3f237
Simone <simonepandolfi@protonmail.com>
2025-07-26 01:52:16 +0200

    initial commit

---
+++ js/game.js
@@ -0,0 +1,27 @@
+import * as canvasUtils from './utils/utils.canvas.js';
+
+let $container = document.getElementById('container');
+
+// Create base game class
+export default class Game {
+    constructor(width, height) {
+        // generateCanvas ritorna sia a canvas che context
+        const { canvas, context } = canvasUtils.generateCanvas(width, height);
+        
+        this.viewport = canvas;
+        this.context = context;
+        this.viewport.id = 'gameViewport';
+
+        // Append the canvas node to our container
+        $container.insertBefore(this.viewport, $container.firstChild);
+
+        // Toss some text into our canvas
+        this.context.font = '64px Arial';
+        this.context.fillText('padre pio un grande padre', 48, 48, 400);
+
+        return this;
+    }
+}
+
+// Instantiate the game in a global
+window.game = new Game(800, 600);
