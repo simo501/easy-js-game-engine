2a135c7f792439368b029b5c7d363563779b6d91
Simone <simonepandolfi@protonmail.com>
2025-07-30 22:31:42 +0200

    tmp saves

--- js/scenes/scene.js
+++ js/scenes/scene.js
@@ -1,7 +1,8 @@
 export class Scene {
-    constructor(scope, endless = false) {
+    constructor(scope, player, endless = false) {
         this.scope = scope;
         this.entities = new Map(); // Mappa per le entità della scena
+        this.player = player; // Il giocatore della scena
         this.endless = endless; // Indica se la scena è infinita o meno
     }
 
